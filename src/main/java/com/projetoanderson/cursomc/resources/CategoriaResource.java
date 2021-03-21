d1893183f18fc3bc9a908e2d3296cceeacadfe18
Anderson Conforto <afcaconforto@gmail.com>
2021-03-21 14:46:59 -0300

    Testando a primeira classe de dominío

--- src/main/java/com/projetoanderson/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/projetoanderson/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,27 @@
 package com.projetoanderson.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.projetoanderson.cursomc.model.CategoriaModel;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST está funcionando!";
+	public List<CategoriaModel> listar() {
+		CategoriaModel cat1 = new CategoriaModel(1,"Informática");
+		CategoriaModel cat2 = new CategoriaModel(2,"Escritório");
+		
+		List<CategoriaModel> lista = new ArrayList<>();
+		lista.add(cat2);
+		lista.add(cat1);
+		return lista;
 	}
 
 }
