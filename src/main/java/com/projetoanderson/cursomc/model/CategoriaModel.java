842db89ba3cb9581888b9a503462fcc30cfde6a7
Anderson Conforto <afcaconforto@gmail.com>
2021-03-21 15:08:18 -0300

    Banco de dados H2 e criacao automatica da base de dados

--- src/main/java/com/projetoanderson/cursomc/model/CategoriaModel.java
+++ src/main/java/com/projetoanderson/cursomc/model/CategoriaModel.java
@@ -2,9 +2,16 @@ package com.projetoanderson.cursomc.model;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class CategoriaModel implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
